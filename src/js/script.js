0410a6089480b0feba2e3bfdfd80d8c53cb7b2db
Vitor.Gomes <santosgomesv@gmail.com>
2022-02-10 01:55:12 -0300

    Merge branch 'main' of https://github.com/santosgv/farma.brasil into main

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,17 @@
 function pesquisarFunc(){
     var pesquisa = document.getElementById("search").value;
-    console.log(pesquisa)
-alert(pesquisa)
+	var div = document.getElementsByClassName("card-title");
+	var tamanho = div.length;
+	
+	
+	
+	for (var i = 0; i < tamanho; i++) {
+         if(div[i].textContent == pesquisa){
+			 alert(div[i].textContent);
+		 }
+		 else{
+			 console.log('Nao foi localizado');
+		 }
 }
-
-function goTop(){
-    $(a).scrollTop()
+	
 }
